3229a356e2e3393cf0c0e7eef9630ab4cde7ad0d
SadieCrapo <scrapo5@byu.edu>
2025-02-19 11:59:14 -0700

    Implement routing

--- src/app.jsx
+++ src/app.jsx
@@ -2,6 +2,55 @@ import React from 'react';
 // import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import { Login } from './login/login';
+import { Backpack } from './backpack/backpack';
+import { Field_Guide } from './field-guide/field-guide';
+import { Greenhouse } from './greenhouse/greenhouse';
+
 export default function App() {
-  return <div className="body bg-dark text-light">App will display here</div>;
+    return (
+        <BrowserRouter>
+            <div className="body bg-dark text-light">
+                <header>
+                    {/* <!-- Navigation elements --> */}
+                    <nav>
+                        <menu>
+                            <li class="nav-item"><NavLink class="nav-link" to="/">Login</NavLink></li>
+                            <li class="nav-item"><NavLink class="nav-link" to="greenhouse">Greenhouse</NavLink></li>
+                            <li class="nav-item"><NavLink class="nav-link" to="field-guide">Field Guide</NavLink></li>
+                            <li class="nav-item"><NavLink class="nav-link" to="backpack">Backpack</NavLink></li>
+                        </menu>
+                    </nav>
+                    <div class="logo">
+                        <span>LOGO</span>
+                    </div>
+                    <div class="welcome-user">
+                        <span>Welcome MysteryUser!</span>
+                    </div>
+
+                </header>
+
+                <Routes>
+                    <Route path='/' element={<Login />} exact />
+                    <Route path='/greenhouse' element={<Greenhouse />} />
+                    <Route path='/field-guide' element={<Field_Guide />} />
+                    <Route path='/backpack' element={<Backpack />} />
+                    <Route path='*' element={<NotFound />} />
+                </Routes>
+
+                <footer>
+                    <div>
+                        <span>Sadie Crapo</span>
+                        <a href="https://github.com/SadieCrapo/Startup" class="text-reset">GitHub</a>
+                    </div>
+                </footer>
+
+            </div>
+        </BrowserRouter>
+    );
 }
+
+function NotFound() {
+    return <main className="container-fluid bg-secondary text-center">404: Return to sender. Address unknown.</main>;
+  }
