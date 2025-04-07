75f6669149607bd744b0baea0224a0fcf3031daa
SadieCrapo <scrapo5@byu.edu>
2025-04-07 15:47:35 -0600

    Fix grid spacing

--- src/backpack/backpack.jsx
+++ src/backpack/backpack.jsx
@@ -8,35 +8,35 @@ export function Backpack({ plantInventory, potInventory, foodInventory }) {
         <main className='backpack-main'>
             <div className="shelves">
                 <div className="item">
-                    <img src="./images/placeholder.png" width="200" alt="A laceleaf plant" />
+                    <img src="./images/placeholder.png" height="200" alt="A laceleaf plant" />
                     <p>Laceleaf x{plantInventory.laceleaf}</p>
                 </div>
                 <div className="item">
-                    <img src="./images/placeholder.png" width="200" alt="A daisy" />
+                    <img src="./images/placeholder.png" height="200" alt="A daisy" />
                     <p>Daisy x{plantInventory.daisy}</p>
                 </div>
                 <div className="item">
-                    <img src="./images/placeholder.png" width="200" alt="A monstera plant" />
+                    <img src="./images/placeholder.png" height="200" alt="A monstera plant" />
                     <p>Monstera x{plantInventory.monstera}</p>
                 </div>
                 <div className="item">
-                    <img src="./images/terracotta-pot.png" width="200" alt="A terracotta pot" />
+                    <img src="./images/terracotta-pot.png" height="200" alt="A terracotta pot" />
                     <p>Terracotta x{potInventory.terracotta}</p>
                 </div>
                 <div className="item">
-                    <img src="./images/placeholder.png" width="200" alt="A hanging planter" />
+                    <img src="./images/placeholder.png" height="200" alt="A hanging planter" />
                     <p>Hanging x{potInventory.hanging}</p>
                 </div>
                 <div className="item">
-                    <img src="./images/placeholder.png" width="200" alt="A marble pot" />
+                    <img src="./images/placeholder.png" height="200" alt="A marble pot" />
                     <p>Marble x{potInventory.marble}</p>
                 </div>
                 <div className="item">
-                    <img src="./images/placeholder.png" width="200" alt="A watering can" />
+                    <img src="./images/placeholder.png" height="200" alt="A watering can" />
                     <p>Water x{foodInventory.water}</p>
                 </div>
                 <div className="item">
-                    <img src="./images/placeholder.png" width="200" alt="A bag of plant food" />
+                    <img src="./images/placeholder.png" height="200" alt="A bag of plant food" />
                     <p>Food x{foodInventory.food}</p>
                 </div>
             </div>
--- src/greenhouse/plant-component.jsx
+++
@@ -1,8 +0,0 @@
-const PlantComponent = ({ plant }) => {
-    return (
-        <div className="plant">
-            <img src={plant.imageUrl} onerror="public/images/placeholder.png" alt="A plant growing in a pot" />
-            <img src="404.png" onerror="this.src='alt.png';" />
-        </div>
-    );
-};
