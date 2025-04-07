038f43f47a87ddfc2a3f810a9eab56647c9dfeb1
SadieCrapo <scrapo5@byu.edu>
2025-04-07 10:46:41 -0600

    Make backpack show realtime inventory

--- src/backpack/backpack.jsx
+++ src/backpack/backpack.jsx
@@ -1,44 +1,45 @@
 import React from 'react';
 import './backpack.css';
 
-export function Backpack() {
-  return (
-    // <main className="container-fluid bg-secondary text-center">
-    <main className='backpack-main'>
-        <div className="shelves">
-            <div className="item">
-                <img alt="A rosebush" />
-                <p>Rose x3</p>
+export function Backpack({ plantInventory, potInventory, foodInventory }) {
+
+    return (
+        // <main className="container-fluid bg-secondary text-center">
+        <main className='backpack-main'>
+            <div className="shelves">
+                <div className="item">
+                    <img src="./images/placeholder.png" width="200" alt="A laceleaf plant" />
+                    <p>Laceleaf x{plantInventory.laceleaf}</p>
+                </div>
+                <div className="item">
+                    <img src="./images/placeholder.png" width="200" alt="A daisy" />
+                    <p>Daisy x{plantInventory.daisy}</p>
+                </div>
+                <div className="item">
+                    <img src="./images/placeholder.png" width="200" alt="A monstera plant" />
+                    <p>Monstera x{plantInventory.monstera}</p>
+                </div>
+                <div className="item">
+                    <img src="./images/terracotta-pot.png" width="200" alt="A terracotta pot" />
+                    <p>Terracotta x{potInventory.terracotta}</p>
+                </div>
+                <div className="item">
+                    <img src="./images/placeholder.png" width="200" alt="A hanging planter" />
+                    <p>Hanging x{potInventory.hanging}</p>
+                </div>
+                <div className="item">
+                    <img src="./images/placeholder.png" width="200" alt="A marble pot" />
+                    <p>Marble x{potInventory.marble}</p>
+                </div>
+                <div className="item">
+                    <img src="./images/placeholder.png" width="200" alt="A watering can" />
+                    <p>Water x{foodInventory.water}</p>
+                </div>
+                <div className="item">
+                    <img src="./images/placeholder.png" width="200" alt="A bag of plant food" />
+                    <p>Food x{foodInventory.food}</p>
+                </div>
             </div>
-            <div className="item">
-                <img alt="A daisy" />
-                <p>Daisy x1</p>
-            </div>
-            <div className="item">
-                <img alt="A haworthia plant" />
-                <p>Haworthia x2</p>
-            </div>
-            <div className="item">
-                <img alt="A monstera plant" />
-                <p>Monstera x4</p>
-            </div>
-            <div className="item">
-                <img src="./images/terracotta-pot.png" width="200" alt="A terracotta pot" />
-                <p>Terracotta x1</p>
-            </div>
-            <div className="item">
-                <img alt="A hanging planter" />
-                <p>Hanging x3</p>
-            </div>
-            <div className="item">
-                <img alt="A marble pot" />
-                <p>Marble x2</p>
-            </div>
-            <div className="item">
-                <img alt="A glass jar" />
-                <p>Jar x1</p>
-            </div>
-        </div>
-    </main>
-  );
+        </main>
+    );
 }
