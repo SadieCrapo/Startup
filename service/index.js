2a575fc68bfcc0b98a9778f2771c220ba6c4a1d4
SadieCrapo <scrapo5@byu.edu>
2025-04-14 16:01:39 -0600

    Implement user creation, login, and logout endpoints

--- service/index.js
+++ service/index.js
@@ -10,9 +10,9 @@ let users = []
 let greenhouses = []
 let plants = {};
 let tasks = {};
-let plantInventory = { 'monstera': 0, 'daisy': 0, 'laceleaf': 0 };
-let potInventory = { 'terracotta': 0, 'marble': 0, 'hanging': 0 };
-let foodInventory = { 'food': 0, 'water': 0 };
+let plantInventory = {};
+let potInventory = {};
+let foodInventory = {};
 
 const port = process.argv.length > 2 ? process.argv[2] : 4000;
 
@@ -26,9 +26,96 @@ app.use(express.static('public'));
 let apiRouter = express.Router();
 app.use(`/api`, apiRouter);
 
-app.get('*', (_req, res) => {
-  res.send({ msg: 'Plantr service' });
+// CreateAuth a new user
+apiRouter.post('/user', async (req, res) => {
+    if (await findUser('userName', req.body.userName)) {
+        res.status(409).send({ msg: 'Existing user' });
+    } else {
+        const user = await createUser(req.body.userName, req.body.password);
+
+        setAuthCookie(res, user.token);
+        setGreenhouseCookie(res, req.body.greenhouse);
+        res.send({ userName: user.userName });
+    }
 });
+
+// GetAuth login an existing user
+apiRouter.post('/session', async (req, res) => {
+    const user = await findUser('userName', req.body.userName);
+    if (user) {
+        if (await bcrypt.compare(req.body.password, user.password)) {
+            user.token = uuid.v4();
+            setAuthCookie(res, user.token);
+            setGreenhouseCookie(res, req.body.greenhouse);
+            res.send({ userName: user.userName });
+            return;
+        }
+    }
+    res.status(401).send({ msg: 'Unauthorized' });
+});
+
+// DeleteAuth logout a user
+apiRouter.delete('/session', async (req, res) => {
+    const user = await findUser('token', req.cookies[authCookieName]);
+    if (user) {
+        delete user.token;
+    }
+    res.clearCookie(authCookieName);
+    res.clearCookie(greenhouseCookieName);
+    res.status(204).end();
+});
+
+// Middleware to verify that the user is authorized to call an endpoint
+const verifyAuth = async (req, res, next) => {
+    const user = await findUser('token', req.cookies[authCookieName]);
+    if (user) {
+        next();
+    } else {
+        res.status(401).send({ msg: 'Unauthorized' });
+    }
+};
+
+// Default error handler
+app.use(function (err, req, res, next) {
+    res.status(500).send({ type: err.name, message: err.message });
+});
+
+// Return the application's default page if the path is unknown
+app.use((_req, res) => {
+    res.sendFile('index.html', { root: 'public' });
+});
+
+async function createUser(userName, password) {
+    const passwordHash = await bcrypt.hash(password, 10);
+
+    const user = {
+        userName: userName,
+        password: passwordHash,
+        token: uuid.v4(),
+    };
+    users.push(user);
+
+    return user;
+}
+
+async function findUser(field, value) {
+    if (!value) return null;
+
+    return users.find((u) => u[field] === value);
+}
+
+// setAuthCookie in the HTTP response
+function setAuthCookie(res, authToken) {
+    res.cookie(authCookieName, authToken, {
+        secure: true,
+        httpOnly: true,
+        sameSite: 'strict',
+    });
+}
+
+// app.get('*', (_req, res) => {
+//   res.send({ msg: 'Plantr service' });
+// });
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
